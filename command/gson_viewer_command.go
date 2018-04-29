d221165ff155d1ac611137c88a109cc847d10050
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-04-29 16:06:04 +0900

    fixed stdout message

--- command/gson_viewer_command.go
+++ command/gson_viewer_command.go
@@ -1,6 +1,8 @@
 package command
 
+import "strings"
+
 // GsonViewerCommand is gson-viewer.nvim command
 func GsonViewerCommand(args []string) (string, error) {
-	return "Hello gsonViewer", nil
+	return "Hello GsonViewer, args: " + strings.Join(args, " "), nil
 }
