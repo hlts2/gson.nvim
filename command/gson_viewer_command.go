90d46257614da117c2b2322352feb66c871c2d0c
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-04-29 18:51:53 +0900

    deleted unnecessary file

--- command/gson_viewer_command.go
+++
@@ -1,8 +0,0 @@
-package command
-
-import "strings"
-
-// GsonViewerCommand is gson-viewer.nvim command
-func GsonViewerCommand(args []string) (string, error) {
-	return "Hello GsonViewer, args: " + strings.Join(args, " "), nil
-}
