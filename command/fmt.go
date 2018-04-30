36c5a1c1a7d064ece07b80d49ac8f9fa9470c7ce
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-04-30 11:24:41 +0900

    added json format command

--- command/fmt.go
+++ command/fmt.go
@@ -1,10 +1,38 @@
 package command
 
 import (
+	"bytes"
+
+	"github.com/hlts2/gson"
 	"github.com/neovim/go-client/nvim"
 )
 
 // Fmt is json format command
 func (gn *GsonNvim) Fmt(v *nvim.Nvim, args []string) error {
-	return v.WriteOut("hello world")
+	vBuf, err := v.CurrentBuffer()
+	if err != nil {
+		return err
+	}
+
+	reader := nvim.NewBufferReader(v, vBuf)
+
+	g, err := gson.NewGsonFromReader(reader)
+	if err != nil {
+		return err
+	}
+
+	vBufCount, err := v.BufferLineCount(vBuf)
+	if err != nil {
+		return err
+	}
+
+	var buf bytes.Buffer
+	err = g.Indent(&buf, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	v.SetBufferLines(vBuf, 0, vBufCount, true, bytes.Split(buf.Bytes(), []byte("\n")))
+
+	return nil
 }
