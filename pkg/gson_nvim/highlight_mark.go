16c3849fbad9d922244935d9d77c3af12ba963a6
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-05-09 11:55:25 +0900

    fixed golint warning

--- pkg/gson_nvim/highlight_mark.go
+++ pkg/gson_nvim/highlight_mark.go
@@ -17,6 +17,7 @@ type HighlightMark struct {
 	end   [2]int
 }
 
+// NewHighlightMark returns HighlightMark instance
 func NewHighlightMark(v *nvim.Nvim, buf nvim.Buffer) (*HighlightMark, error) {
 	start, err := v.BufferMark(buf, StartHighlightMark)
 	if err != nil {
@@ -36,6 +37,7 @@ func NewHighlightMark(v *nvim.Nvim, buf nvim.Buffer) (*HighlightMark, error) {
 	return hm, nil
 }
 
+// AdjustMarkWithRange adjusts mark with range
 func (hm *HighlightMark) AdjustMarkWithRange(startLineNum, endLineNum int) {
 	if hm.start[0] != startLineNum || hm.end[0] != endLineNum {
 		hm.start[0] = startLineNum
@@ -46,26 +48,32 @@ func (hm *HighlightMark) AdjustMarkWithRange(startLineNum, endLineNum int) {
 	}
 }
 
+// GetStart returns line number and line position of highlight start mark.
 func (hm *HighlightMark) GetStart() [2]int {
 	return hm.start
 }
 
+// GetStartLineNum returns line number of highlight start mark
 func (hm *HighlightMark) GetStartLineNum() int {
 	return hm.start[0]
 }
 
+// GetStartPos returns line position of highlight start mark
 func (hm *HighlightMark) GetStartPos() int {
 	return hm.start[1]
 }
 
+// GetEnd returns line number and line position of highlight end mark.
 func (hm *HighlightMark) GetEnd() [2]int {
 	return hm.end
 }
 
+// GetEndLineNum returns line number of highlight end mark
 func (hm *HighlightMark) GetEndLineNum() int {
 	return hm.end[0]
 }
 
+// GetEndPos returns line position of highlight end mark
 func (hm *HighlightMark) GetEndPos() int {
 	return hm.end[1]
 }
